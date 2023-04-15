05b7c2d06409ae50490699c6e9470c46eb1a78f5
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-04-15 19:02:14 -0300

    creacion de la nueva pagina de inicio

--- santiagoalvarez/src/App.js
+++ santiagoalvarez/src/App.js
@@ -1,18 +1,6 @@
 import style from "./App.module.css"
-import {useEffect, useRef, useState} from "react"
-import fotodeperfil from "./assets/fotodeperfil.jpeg"
 import Animations from "./components/Animations"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin, } from "@fortawesome/free-brands-svg-icons";
-import Skills from "./components/skills/Skills"
-import Projects from "./components/projects/Projects"
-
-//hace con que cuando aparezca la pagina dos aparezca la barra de navegacion
-// el elemento HTML, una instancia de Insertio.. , y un estado local al hook
-
-
-
+import Presentation from "./components/Presentation/Presentation"
 
 function App() {
   
@@ -21,41 +9,8 @@ function App() {
 
 return(
   <div className={style.app}>
-    <div className={style.boxOne} >
-      <div>
-        <div className={style.rowUp}></div>
-        <div className={style.rowBootom}></div>
-      </div>
-    </div>
-
-    <div className={style.boxTwo} >
-      
-    </div>
-
-    <div className={style.boxThree} >
-      <div className={style.marco_izq}>
-        <div>
-          <div className={style.rowUp}></div>
-          <div className={style.rowBootom}></div>
-        </div>
-      </div>
-
-      <div className={style.container_nav}>
-        <div className={style.nav}>
-          <div><h3>ABOUT ME</h3> </div>
-          <div><h3>PROJECTS</h3> </div>
-          <div><h3>SKILLS</h3> </div>
-          <div><h3>CONTAC ME</h3> </div>
-        </div>
-      </div>
-
-      <div className={style.marco_drc}>
-          <div>
-            <div className={style.rowBootom_d}></div>
-            <div className={style.rowUp_d}></div>
-          </div>
-      </div>
-    </div>  
+    <Presentation />
+    
   </div>)
 }
 
